8e6b5ac190499a9a44a932841cbd6f55190c3b6a
yukiko-m <36286017+yukiko-m@users.noreply.github.com>
2018-02-23 14:02:11 +0900

    Create logout.java

---
+++ logout.java
@@ -0,0 +1,12 @@
+import javax.servlet.http.HttpSession;
+
+import java.sql.Statement;
+
+class logout extends Executer {
+	public void execute(ServletRequest req) throws Exception{
+        
+		HttpSession session = req.getRequest().getSession();
+		
+		session.removeAttribute("s_id"); 
+	}
+}
